26f3b1af41a3b79ae6a0f9ae659d611dde38381b
Lennygonz <lenny.gonzaleznyc@gmail.com>
2018-05-19 00:25:57 -0400

    this helper file is for inputting/outputting the sample pascal code

---
+++ Compiler/Pascal Helper Files/pascalreader.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+import os
+
+PASCAL_FILE_EXT = '.pas'
+
+
+class PascalFile(object):
+    def __init__(self, input_file_location, output_location):
+        """
+        :param input_file_location: str
+        :param output_location: str
+        :return:
+        """
+
+        self.input_file_location = os.path.join('Pascal Code Examples', input_file_location)
+        self.output_file_location = output_location
+        self.FILE = open(self.input_file_location)
+
+        self.contents = self.FILE.read()
+
+    def io_object(self):
+        return self.FILE
+
+    def __unicode__(self):
+        return self.input_file_location
+
+    def __del__(self):
+        self.FILE.close()
